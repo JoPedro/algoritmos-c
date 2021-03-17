b91b70f6f7a053d1c4f5af1904fcd7ca3f97a763
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-03-16 22:55:19 -0300

    Finished subseqMaiorInArray

--- subseqMaiorInArray.c
+++ subseqMaiorInArray.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int subseqMaiorInArray(int[], int);
+int subseqMaiorInArray_(int[], int, int);
 
 int main() {
     int n;
@@ -8,8 +9,30 @@ int main() {
     int arr[n];
     for (int i = 0; i < n; ++i)
         scanf("%d", &arr[i]);
+    printf("%d\n", subseqMaiorInArray(arr, n));
+    return 0;
 }
 
 int subseqMaiorInArray(int arr[], int n) {
-    for (int i = 0; i < sub; ++i)
+    subseqMaiorInArray_(arr, n, n);
+}
+
+int subseqMaiorInArray_(int arr[], int n, int sub) {
+    static int soma = 0;
+    if (sub >= 2) {
+        int aux = 0;
+
+        for (int i = 0; i <= (n - sub); ++i) {
+            for (int j = i; j < (i + sub); ++j) {
+                aux += arr[j];
+            }
+            if (aux > soma)
+                soma = aux;
+            aux = 0;
+        }
+        
+        --sub;
+        return subseqMaiorInArray_(arr, n, sub);
+    }
+    return soma;
 }
