200b5e6a910674dc21ccc5526b9307473a8a950c
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-04-11 09:07:34 -0300

    8queens almost (up)

--- 8queens.c
+++ 8queens.c
@@ -3,7 +3,7 @@
 /* Expotential(2^n) */
 
 int eightQueens(char[8][8]);
-int eightQueensRecur(char[8][8], int, int);
+int eightQueensRecur(char[8][8], int);
 int isPossible(char[8][8], int, int);
 
 int main() {
@@ -11,7 +11,7 @@ int main() {
     int i, j;
     for (i = 0; i < 8; ++i) {
         for (j = 0; j < 8; ++j) {
-            scanf(" %c", &tab[i][j]);
+            tab[i][j] = '-';
         }
     }
     eightQueens(tab);
@@ -37,25 +37,27 @@ int isPossible(char arr[8][8], int linha, int col)
 }
 
 int eightQueens(char arr[8][8]) {
-    return eightQueensRecur(arr, 0, 0);
+    return eightQueensRecur(arr, 0);
 }
 
-int eightQueensRecur(char arr[8][8], int linha, int col) {  
+int eightQueensRecur(char arr[8][8], int linha) {  
+    /* Aceita */
+    if (linha == 8) return 1;
     /* Rejeita */
-    int c, exists = 0;
+    int c, col, exists = 0;
     for (c = 0; c < 8; ++c) {
         if (arr[linha][c] != 'Q') exists = 0;
         else {
             arr[linha][c] = '-';
             exists = 1;
-            col = c;
+            col = c+1;
             break;
         }
     }
-    if (exists == 0) col = -1;
+    if (exists == 0) col = 0;
     int column;
     int possible = 0;
-    for (column = col+1; column < 8; ++column) {
+    for (column = col; column < 8; ++column) {
         if (isPossible(arr, linha, column)) {
             possible = 1;
             col = column;
@@ -63,11 +65,9 @@ int eightQueensRecur(char arr[8][8], int linha, int col) {
         }
     }
     if (possible == 0) return 0;
-    /* Aceita */
     arr[linha][col] = 'Q';
-    if (linha == 7 && possible == 1) return 1;
     /* Chamadas Recursivas */
-    int ans = eightQueensRecur(arr, linha+1, col);
-    if (!ans) ans = eightQueensRecur(arr, linha-1, col);
+    int ans = eightQueensRecur(arr, linha+1);
+    if (!ans) ans = eightQueensRecur(arr, linha-1);
     return ans;
 }
